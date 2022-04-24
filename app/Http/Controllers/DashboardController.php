40d4f9d5288237d50283a511c9f948ea35266d11
Saif Refaye <saife.refaye552@outlook.com>
2022-04-24 22:39:24 +0200

    first commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        if(Auth::user()->hasRole('user'))
+        {
+            return view('userdah');
+        }elseif(Auth::user()->hasRole('Admin'))
+        {
+            return view('admindash');
+        }
+    }
+}
